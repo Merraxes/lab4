481df3b4aa68556b373055fc36fecd56f4b6f34e
Merraxes <bielecki_f@o2.pl>
2016-04-21 00:06:53 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
-#include <stdio.h>
+#include <iostream>
+#include <cstdlib>
+using namespace std;
 
-int main(int argc, char *argv[])
-{
-	printf("Hello, World!\n");
+void showNumerals(int &n);
+
+int main(int argc, char const *argv[]) {
+
+    int n = atoi(argv[1]);
+    int k=0, temp, nk;
+    bool found=false;
+    do
+    {
+        found=true;
+        ++k;
+        nk=n*k;
+        temp=nk;
+        while (temp>1)
+        {
+            if ((temp % 10) == 0) temp=temp/10;
+            else{
+                 found=false;
+                 break;}
+        }
+    }while (found==false);
+    cout<<k;
 }
